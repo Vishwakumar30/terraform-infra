cd342c8eef9a2e8a5b1dc7483d0c96147170cd9e
VISHWA KUMAR K <kumarkumar8681@gmail.com>
2026-07-11 11:11:37 +0530

    Refactor RDS module and update instance settings
    Updated RDS module configuration with new parameters and removed deprecated resources.

--- modules/rds.tf
+++ modules/rds.tf
@@ -1,62 +1,17 @@
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
 
-resource "aws_db_subnet_group" "my-db-subnet-group" {
-  name = "my-db-subnet-group"
+  identifier = "my-rds"
 
-  subnet_ids = [
-    aws_subnet.private-subnet.id,
-    aws_subnet.private-subnet-2.id
-  ]
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
 
-  tags = {
-    Name = "my-db-subnet-group"
-  }
-}
-resource "aws_security_group" "rds-sg" {
-  name        = "rds-sg"
-  description = "Security Group for RDS"
-  vpc_id      = aws_vpc.my-vpc-1.id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "rds-sg"
-  }
-}
-resource "aws_db_instance" "my-rds" {
-  identifier             = "my-rds"
-  allocated_storage      = 20
-  storage_type           = "gp3"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
-
-  db_name  = "mydatabase"
+  db_name  = "mydb"
   username = "admin"
-  password = "Password@123"
+  password = "ChangeMe123!"
 
-  publicly_accessible    = false
-  skip_final_snapshot    = true
-  deletion_protection    = false
-
-  vpc_security_group_ids = [
-    aws_security_group.rds-sg.id
-  ]
-
-  db_subnet_group_name = aws_db_subnet_group.my-db-subnet-group.name
-
-  tags = {
-    Name = "my-rds"
-  }
+  publicly_accessible = false
+  skip_final_snapshot = true
 }
