418f7d421af1f5c3754c014526f64834f5a8f96c
VISHWA KUMAR K <kumarkumar8681@gmail.com>
2026-07-13 08:32:04 +0530

    Define variables for AWS and PostgreSQL settings
    Added variables for AWS region, project name, environment, and PostgreSQL database configuration.

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,43 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "microservices-demo"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_name" {
+  description = "PostgreSQL database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "PostgreSQL username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "PostgreSQL password"
+  type        = string
+  sensitive   = true
+}
+
+4. terraform.tfvars
+aws_region   = "ap-south-1"
+project_name = "microservices-demo"
+environment  = "dev"
+
+db_name     = "microservices"
+db_username = "postgres"
+db_password = "ChangeMe123!"
